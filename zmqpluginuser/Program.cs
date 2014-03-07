61728b98c1cfbddec27816389ef15da73d66c53e
Dmitry Ledentsov <dmlled@yahoo.com>
2014-03-07 19:55:44 +0100

    extract plugin

--- zmqpluginuser/Program.cs
+++ zmqpluginuser/Program.cs
@@ -9,25 +9,20 @@ using System.Threading.Tasks;
 namespace zmqpluginuser
 {
     class Program {
-
-        delegate int plugin_loader([MarshalAs(UnmanagedType.LPStr)] string config);
-
         static void Main(string[] args)
         {
             try
             {
-                using (UnmanagedLibrary my_plugin = new UnmanagedLibrary("zmqplugin.dll"))
+                using (var my_plugin = new zmq_plugin("zmqplugin.dll"))
                 {
-                    var load_plugin = my_plugin.GetUnmanagedFunction<plugin_loader>("load_plugin");
-                    if (load_plugin != null)
-                        Console.WriteLine(load_plugin("bla"));
+                    Console.WriteLine(my_plugin.load("bla"));
                 }
+
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
-
         }
     }
 }
---
+++ zmqpluginuser/zmq_plugin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zmqpluginuser
+{
+    public class zmq_plugin : IDisposable
+    {
+        public delegate int plugin_loader_delegate([MarshalAs(UnmanagedType.LPStr)] string config);
+        UnmanagedLibrary my_plugin;
+        plugin_loader_delegate plugin_loader;
+        
+        public int load(string config) {
+            return plugin_loader(config);
+        }
+
+        public zmq_plugin(string zmq_plugin_dll)
+        {
+            my_plugin = new UnmanagedLibrary(zmq_plugin_dll);
+            plugin_loader = my_plugin.GetUnmanagedFunction<plugin_loader_delegate>("load_plugin");
+        }
+
+        public void Dispose()
+        {
+            if (my_plugin != null)
+                my_plugin.Dispose();
+        }
+    }
+}
