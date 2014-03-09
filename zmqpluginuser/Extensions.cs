114e614f4fb747566f14d97d820a97abed8bd35e
Dmitry Ledentsov <dmlled@yahoo.com>
2014-03-09 19:19:00 +0100

    simple code shoveling

---
+++ zmqpluginuser/Extensions.cs
@@ -0,0 +1,66 @@
+﻿using MsgPack.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zmqpluginuser
+{
+    static class Extensions
+    {
+        public static byte[] ToByteArray(this string str)
+        {
+            byte[] bytes = new byte[str.Length * sizeof(char)];
+            System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+            return bytes;
+        }
+
+        public static string ToStringFromByteArray(this byte[] bytes)
+        {
+            char[] chars = new char[bytes.Length / sizeof(char)];
+            System.Buffer.BlockCopy(bytes, 0, chars, 0, bytes.Length);
+            return new string(chars);
+        }
+
+        public static byte[] PackMsg<T>(T t)
+        {
+            var serializer = MessagePackSerializer.Create<T>();
+            return serializer.PackSingleObject(t);
+        }
+
+        public static byte[] Pack<T>(this T t)
+        {
+            return PackMsg(t);
+        }
+
+        public static T UnpackMsg<T>(byte[] t)
+        {
+            var serializer = MessagePackSerializer.Create<T>();
+            return serializer.UnpackSingleObject(t);
+        }
+
+        public static T Unpack<T>(this byte[] t)
+        {
+            var serializer = MessagePackSerializer.Create<T>();
+            return serializer.UnpackSingleObject(t);
+        }
+
+        public static byte[] ToASCII(this string s)
+        {
+            return System.Text.Encoding.ASCII.GetBytes(s);
+        }
+
+        /// <summary>
+        /// http://stackoverflow.com/a/969327/847349
+        /// </summary>
+        public static TimeSpan Time(Action action)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            action();
+            stopwatch.Stop();
+            return stopwatch.Elapsed;
+        }
+    }
+}
--- zmqpluginuser/Program.cs
+++ zmqpluginuser/Program.cs
@@ -11,63 +11,6 @@ using System.Threading.Tasks;
 
 namespace zmqpluginuser
 {
-
-    static class Extensions
-    {
-        public static byte[] ToByteArray(this string str)
-        {
-            byte[] bytes = new byte[str.Length * sizeof(char)];
-            System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
-            return bytes;
-        }
-
-        public static string ToStringFromByteArray(this byte[] bytes)
-        {
-            char[] chars = new char[bytes.Length / sizeof(char)];
-            System.Buffer.BlockCopy(bytes, 0, chars, 0, bytes.Length);
-            return new string(chars);
-        }
-
-        public static byte[] PackMsg<T>(T t)
-        {
-            var serializer = MessagePackSerializer.Create<T>();
-            return serializer.PackSingleObject(t);
-        }
-
-        public static byte[] Pack<T>(this T t)
-        {
-            return PackMsg(t);
-        }
-
-        public static T UnpackMsg<T>(byte[] t)
-        {
-            var serializer = MessagePackSerializer.Create<T>();
-            return serializer.UnpackSingleObject(t);
-        }
-
-        public static T Unpack<T>(this byte[] t)
-        {
-            var serializer = MessagePackSerializer.Create<T>();
-            return serializer.UnpackSingleObject(t);
-        }
-
-        public static byte[] ToASCII(this string s)
-        {
-            return System.Text.Encoding.ASCII.GetBytes(s);
-        }
-
-        /// <summary>
-        /// http://stackoverflow.com/a/969327/847349
-        /// </summary>
-        public static TimeSpan Time(Action action)
-        {
-            Stopwatch stopwatch = Stopwatch.StartNew();
-            action();
-            stopwatch.Stop();
-            return stopwatch.Elapsed;
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
@@ -87,66 +30,16 @@ namespace zmqpluginuser
                     switch (load_result)
                     {
                         case zmqpluginuser.load_result.OK:
-                            Console.WriteLine("Hello {0}", my_plugin.hello());
-                            var res = my_plugin.call(
-                            new call
-                            {
-                                name = "add".ToASCII(),
-                                parameters = {
-                                    new List<int>{2,3,5}.Pack()
-                                }
-                            }.Pack());
-                            int call_res = Extensions.UnpackMsg<int>(res);
-                            Console.WriteLine("Result: {0}", call_res);
+                            var res = SimpleExample(my_plugin);
 
                             // wrong call demo
-                            res = my_plugin.call(new call { name = "bla".ToASCII() }.Pack() );
-                            string err_res = System.Text.Encoding.ASCII.GetString(Extensions.UnpackMsg<byte[]>(res));
-                            Console.WriteLine(err_res);
-                            res = my_plugin.call(new call { name = "add".ToASCII() }.Pack());
-                            err_res = System.Text.Encoding.ASCII.GetString(Extensions.UnpackMsg<byte[]>(res));
-                            Console.WriteLine(err_res);
+                            res = TryWrongCalls(my_plugin, res);
 
                             // overhead measurement
                             int count = 10000000;
                             var numbers = Enumerable.Range(1, count).Reverse().ToList();
-                            Console.WriteLine("benchmarking communication [{0}]", count);
-                            Console.WriteLine("Message length: {0} MB",
-                            new call {
-                                name = "sort_max".ToASCII(),
-                                parameters = { numbers.Pack() }
-                            }.Pack().Length / 1048576f);
-                            var benchmark_call = new call
-                                        {
-                                            name = "sort_max".ToASCII(),
-                                            parameters = {
-                                                numbers.Pack()
-                                            }
-                                        }.Pack();
-                            for (int i = 0; i < 3; i++)
-                            {
-                                Console.WriteLine(Extensions.Time(() =>
-                                {
-                                    var _ = my_plugin.call(benchmark_call);
-                                }));
-                            }
-
-                            // echo measurement
-                            Console.WriteLine("benchmarking echo+deserialization...");
-                            var echo_call = new call
-                                        {
-                                            name = "echo".ToASCII(),
-                                            parameters = {
-                                                numbers.Pack()
-                                            }
-                                        }.Pack();
-                            for (int i = 0; i < 3; i++)
-                            {
-                                Console.WriteLine(Extensions.Time(() =>
-                                {
-                                    var _ = my_plugin.call(echo_call);
-                                }));
-                            }
+                            MeasureSortMax(my_plugin, count, numbers);
+                            MeasureEcho(my_plugin, numbers);
                             break;
                         case zmqpluginuser.load_result.FAILED_CONNECTING:
                             Console.WriteLine("failed connecting");
@@ -161,6 +54,79 @@ namespace zmqpluginuser
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
+            }
+        }
+
+        private static byte[] SimpleExample(zmq_plugin my_plugin)
+        {
+            Console.WriteLine("Hello {0}", my_plugin.hello());
+            var res = my_plugin.call(
+            new call
+            {
+                name = "add".ToASCII(),
+                parameters = {
+                                    new List<int>{2,3,5}.Pack()
+                                }
+            }.Pack());
+            int call_res = Extensions.UnpackMsg<int>(res);
+            Console.WriteLine("Result: {0}", call_res);
+            return res;
+        }
+
+        private static byte[] TryWrongCalls(zmq_plugin my_plugin, byte[] res)
+        {
+            res = my_plugin.call(new call { name = "bla".ToASCII() }.Pack());
+            string err_res = System.Text.Encoding.ASCII.GetString(Extensions.UnpackMsg<byte[]>(res));
+            Console.WriteLine(err_res);
+            res = my_plugin.call(new call { name = "add".ToASCII() }.Pack());
+            err_res = System.Text.Encoding.ASCII.GetString(Extensions.UnpackMsg<byte[]>(res));
+            Console.WriteLine(err_res);
+            return res;
+        }
+
+        private static void MeasureSortMax(zmq_plugin my_plugin, int count, List<int> numbers)
+        {
+            Console.WriteLine("benchmarking communication [{0}]", count);
+            Console.WriteLine("Message length: {0} MB",
+            new call
+            {
+                name = "sort_max".ToASCII(),
+                parameters = { numbers.Pack() }
+            }.Pack().Length / 1048576f);
+            var benchmark_call = new call
+            {
+                name = "sort_max".ToASCII(),
+                parameters = {
+                                                numbers.Pack()
+                                            }
+            }.Pack();
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(Extensions.Time(() =>
+                {
+                    var _ = my_plugin.call(benchmark_call);
+                }));
+            }
+        }
+
+        private static void MeasureEcho(zmq_plugin my_plugin, List<int> numbers)
+        {
+
+            // echo measurement
+            Console.WriteLine("benchmarking echo+deserialization...");
+            var echo_call = new call
+            {
+                name = "echo".ToASCII(),
+                parameters = {
+                                                numbers.Pack()
+                                            }
+            }.Pack();
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(Extensions.Time(() =>
+                {
+                    var _ = my_plugin.call(echo_call);
+                }));
             }
         }
     }
